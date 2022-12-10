dc0fc62bad6e99a3b4546b701021b2db02e3b145
Nguyễn Tuấn <nhtuan212@gmail.com>
2022-12-11 01:01:24 +0700

    #updated deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 // const methodOverride = require('method-override')
 const { create } = require('express-handlebars');
 const { route } = require('./routes');
