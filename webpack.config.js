9489234adbfc0d675f64fe84d13f414b8525adaa
Nguyễn Tuấn <nhtuan212@gmail.com>
2022-12-11 11:53:17 +0700

    #updated deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-	entry: 'src/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist'),
 		filename: 'my-first-webpack.bundle.js',
+	},
+	module: {
+		rules: [{ test: /\.txt$/, use: 'raw-loader' }],
 	},
 };
