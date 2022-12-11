f9cd889cb0384eb59d5918ba13831c3ef56ae0f9
Nguyễn Tuấn <nhtuan212@gmail.com>
2022-12-11 11:52:27 +0700

    #updated deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,9 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-const path = require('path')
+const path = require('path');
 
-module.exports = async function (env, argv) {
-	const config = await createExpoWebpackConfigAsync(env, argv);
-	config.module.rules.forEach(r => {
-		if (r.oneOf) {
-			r.oneOf.forEach(o => {
-				if (o.use && o.use.loader && o.use.loader.includes('babel-loader')) {
-					o.include = [
-						path.resolve('.'),
-						path.resolve('node_modules/@ui-kitten/components'),
-					]
-				}
-			})
-		}
-	})
-	return config;
+module.exports = {
+	entry: 'src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'my-first-webpack.bundle.js',
+	},
 };
