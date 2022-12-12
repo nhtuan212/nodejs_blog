5d42c1988cdf13448e89ef1da60ad1b03ca745b2
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2022-12-12 11:44:46 +0700

    #updated deploy

--- src/api.js
+++ src/api.js
@@ -33,9 +33,9 @@ router.get("/", (req, res, next) => {
 // app.use(`/`, router);
 app.use(`/.netlify/functions/api`, router);
 
-app.listen(port, () => {
-	console.log(`App listening on port ${port}`);
-});
+// app.listen(port, () => {
+// 	console.log(`App listening on port ${port}`);
+// });
 
 module.exports = app;
 module.exports.handler = serverless(app);
