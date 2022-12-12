ee016b7a2414961b3d5944ebe744c49b4852024c
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2022-12-12 13:51:04 +0700

    #updated deploy

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const serverless = require("serverless-http");
+// const express = require("express");
+// const serverless = require("serverless-http");
 // const { connectDB } = require('./config/db');
 
-// import express from 'express';
-// import serverless from 'serverless-http';
-// import { connectDB } from './config/db';
-// import Course from './app/models/Course';
+import express from 'express';
+import serverless from 'serverless-http';
+import { connectDB } from './config/db';
+import Course from './app/models/Course';
 
 const app = express();
 const router = express.Router();
@@ -13,18 +13,14 @@ const router = express.Router();
 const port = 3000;
 
 // Connect MongoDB
-// connectDB('mongodb+srv://blog_database:123@cluster0.aipk5ey.mongodb.net/bin_database')
+connectDB('mongodb+srv://blog_database:123@cluster0.aipk5ey.mongodb.net/bin_database')
 
 router.get("/", (req, res, next) => {
-	return res.json({
-		name: 'binayu'
-	})
-
-	// Course.find({})
-	// 	.then((courses) => {
-	// 		return res.json(courses);
-	// 	})
-	// 	.catch(next);
+	Course.find({})
+		.then((courses) => {
+			return res.json(courses);
+		})
+		.catch(next);
 });
 app.use(`/.netlify/functions/api`, router);
 
