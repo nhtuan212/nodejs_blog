93ea1ed6e144bbcb3cb24265b74621736e6b7a68
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2022-12-12 09:20:36 +0700

    test deploy

--- src/api.js
+++ src/api.js
@@ -1,14 +1,26 @@
+require('dotenv').config();
 const express = require("express");
 const serverless = require("serverless-http");
 const { port, ext, mongoURL } = require('./config/constants');
+const { connectDB } = require('./config/db');
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.json({
-		hello: "hi!"
-	});
+const Course = require('./app/models/Course');
+
+// Connect MongoDB
+connectDB(mongoURL)
+
+router.get("/", (req, res, next) => {
+	Course.find({})
+		.then((courses) => {
+			console.log({
+				courses
+			});
+			return res.json(courses);
+		})
+		.catch(next);
 });
 
 // app.use(`/`, router);
