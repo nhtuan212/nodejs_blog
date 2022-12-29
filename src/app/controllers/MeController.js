a8fedfb4912dd577d0f572cb57a6a6aeeb4bd885
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2022-12-29 14:01:52 +0700

    #test husky

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,69 +1,62 @@
-const Course = require('../models/Course');
-const { multipleMongooseToObject } = require('../../util/mongoose');
+const Course = require('../models/Course')
+const { multipleMongooseToObject } = require('../../util/mongoose')
 
 class MeController {
+    // [GET]/me/stored/courses
+    storedCourses = (req, res, next) => {
+        let courseQuery = Course.find({})
 
-	// [GET]/me/stored/courses
-	storedCourses = (req, res, next) => {
-		let courseQuery = Course.find({})
+        if (req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            })
+        }
 
-		if (req.query.hasOwnProperty('_sort')) {
-			courseQuery = courseQuery.sort({
-				[req.query.column]: req.query.type
-			})
-		}
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) =>
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses),
+                }),
+            )
+            .catch(next)
 
-		Promise.all([
-			courseQuery,
-			Course.countDocumentsDeleted()
-		])
-			.then(([courses, deletedCount]) => 
-				res.render('me/stored-courses', {
-					deletedCount,
-					courses: multipleMongooseToObject(courses)
-				})
-			)
-			.catch(next)
+        // Course.countDocumentsDeleted()
+        // 	.then(deletedCount => {
+        // 		console.log({
+        // 			deletedCount
+        // 		});
+        // 	})
+        // 	.catch(next)
 
-		// Course.countDocumentsDeleted()
-		// 	.then(deletedCount => {
-		// 		console.log({
-		// 			deletedCount
-		// 		});
-		// 	})
-		// 	.catch(next)
+        // Course.find({})
+        // 	.then(courses => res.render('me/stored-courses',{
+        // 		courses: multipleMongooseToObject(courses)
+        // 	}))
+        // 	.catch(next)
+    }
 
-		// Course.find({})
-		// 	.then(courses => res.render('me/stored-courses',{
-		// 		courses: multipleMongooseToObject(courses)
-		// 	}))
-		// 	.catch(next)
-	}
+    // [GET]/me/stored/courses-api
+    storedCoursesApi = (req, res, next) => {
+        let courseQuery = Course.find({})
 
-	// [GET]/me/stored/courses-api
-	storedCoursesApi = (req, res, next) => {
-		let courseQuery = Course.find({})
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
+            .then(() => res.json(req.courses))
+            .catch(next)
+    }
 
-		Promise.all([
-			courseQuery,
-			Course.countDocumentsDeleted()
-		])
-			.then(() => 
-				res.json(req.courses)
-			)
-			.catch(next)
-	}
-
-	// [GET]/me/trash/courses
-	trashCourses = (req, res, next) => {
-		Course.findDeleted({})
-			.then(courses => res.render('me/trash-courses',{
-				courses: multipleMongooseToObject(courses)
-			}))
-			.catch(next)
-	}
+    // [GET]/me/trash/courses
+    trashCourses = (req, res, next) => {
+        Course.findDeleted({})
+            .then(courses =>
+                res.render('me/trash-courses', {
+                    courses: multipleMongooseToObject(courses),
+                }),
+            )
+            .catch(next)
+    }
 }
 
 module.exports = {
-	meController: new MeController(),
-};
+    meController: new MeController(),
+}
