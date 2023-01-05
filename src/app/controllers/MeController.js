acbaaccc24566f81c709a8ac04fe6b4d3bb7fe4a
Nguyen Hoang Tuan <tuan.nguyen.hoang@vieon.vn>
2023-01-05 14:56:11 +0700

    #test husky

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,25 +1,25 @@
-const Course = require('../models/Course')
-const { multipleMongooseToObject } = require('../../util/mongoose')
+const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class MeController {
     // [GET]/me/stored/courses
     storedCourses = (req, res, next) => {
-        let courseQuery = Course.find({})
+        let courseQuery = Course.find({});
 
         if (req.query.hasOwnProperty('_sort')) {
             courseQuery = courseQuery.sort({
-                [req.query.column]: req.query.type
-            })
+                [req.query.column]: req.query.type,
+            });
         }
 
         Promise.all([courseQuery, Course.countDocumentsDeleted()])
             .then(([courses, deletedCount]) =>
                 res.render('me/stored-courses', {
                     deletedCount,
-                    courses: multipleMongooseToObject(courses)
-                })
+                    courses: multipleMongooseToObject(courses),
+                }),
             )
-            .catch(next)
+            .catch(next);
 
         // Course.countDocumentsDeleted()
         // 	.then(deletedCount => {
@@ -34,29 +34,29 @@ class MeController {
         // 		courses: multipleMongooseToObject(courses)
         // 	}))
         // 	.catch(next)
-    }
+    };
 
     // [GET]/me/stored/courses-api
     storedCoursesApi = (req, res, next) => {
-        let courseQuery = Course.find({})
-
-        Promise.all([courseQuery, Course.countDocumentsDeleted()])
-            .then(() => res.json(req.courses))
-            .catch(next)
-    }
+        Course.find({})
+            .then(courses => {
+                return res.json(courses);
+            })
+            .catch(next);
+    };
 
     // [GET]/me/trash/courses
     trashCourses = (req, res, next) => {
         Course.findDeleted({})
-            .then((courses) =>
+            .then(courses =>
                 res.render('me/trash-courses', {
-                    courses: multipleMongooseToObject(courses)
-                })
+                    courses: multipleMongooseToObject(courses),
+                }),
             )
-            .catch(next)
-    }
+            .catch(next);
+    };
 }
 
 module.exports = {
-    meController: new MeController()
-}
+    meController: new MeController(),
+};
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,26 +1,29 @@
-const mongoose = require('mongoose')
-const slug = require('mongoose-slug-generator')
-const mongooseDelete = require('mongoose-delete')
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const Course = new Schema({
-	_id: { type: Number, },
-	name: { type: String, default: '', required: true, maxLength: 255 },
-    description: { type: String, default: '', maxLength: 600 },
-    image: { type: String, default: '', maxLength: 255 },
-    slug: { type: String, default: '', maxLength: 50, slug: 'name', unique: true },
-    deletedAt: { type: String, default: null, maxLength: 50 }
-}, {
-	_id: false,
-	timestamps: true
-})
+const Course = new Schema(
+    {
+        _id: { type: Number },
+        name: { type: String, default: '', required: true, maxLength: 255 },
+        description: { type: String, default: '', maxLength: 600 },
+        image: { type: String, default: '', maxLength: 255 },
+        slug: { type: String, default: '', maxLength: 50, slug: 'name', unique: true },
+        deletedAt: { type: String, default: null, maxLength: 50 },
+    },
+    {
+        _id: false,
+        timestamps: true,
+    },
+);
 
 // Add Plugin
-mongoose.plugin(slug)
-Course.plugin(mongooseDelete, { overrideMethods: 'all' })
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, { overrideMethods: 'all' });
 Course.plugin(AutoIncrement);
 // Course.plugin(AutoIncrement, { inc_field: 'name' });
 
-module.exports = mongoose.model('products', Course)
+module.exports = mongoose.model('products', Course);
